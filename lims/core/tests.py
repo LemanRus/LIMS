5cdf199780fbdc4ceeb94ecfb88c0592bf52028a
LemanRus <wlad163163@gmail.com>
2023-02-03 10:01:31 +0400

    Add simple tests

--- lims/core/tests.py
+++ lims/core/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class WalkAroundCase(TestCase):
+    def test_home_page_response(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_help_page_response(self):
+        response = self.client.get('/help/')
+        self.assertEqual(response.status_code, 200)
