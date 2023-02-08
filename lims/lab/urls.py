fa593b0c0ba19c8aa443c737b6d6d20320c6947c
LemanRus <wlad163163@gmail.com>
2023-02-08 16:07:48 +0400

    Add detailed views to reagents and methodics

--- lims/lab/urls.py
+++ lims/lab/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('', TemplateView.as_view(), name='home'),
     path('reagents/', views.ReagentsListView.as_view(), name='reagents'),
     path('methodics/', views.MethodicsListView.as_view(), name='methodics'),
+    path('reagents/<int:reagent_id>/', views.ReagentDetailView.as_view(), name='reagent_detail'),
+    path('methodics/<int:methodic_id>/', views.MethodicDetailView.as_view(), name='methodic_detail'),
 ]
--- lims/lab/views.py
+++ lims/lab/views.py
@@ -15,14 +15,16 @@ class MethodicsListView(ListView):
     template_name = "lab/methodics_list.html"
     context_object_name = "methodics"
 
-#
-# class ReagentDetailView(DetailView):
-#     model = Reagent
-#     template_name = "lab/reagents_list.html"
-#     context_object_name = "reagents"
-#
-#
-# class MethodicDetailView(DetailView):
-#     model = Methodic
-#     template_name = "lab/methodics_list.html"
-#     context_object_name = "methodics"
+
+class ReagentDetailView(DetailView):
+    model = Reagent
+    template_name = "lab/reagent_detail.html"
+    context_object_name = "reagent"
+    pk_url_kwarg = "reagent_id"
+
+
+class MethodicDetailView(DetailView):
+    model = Methodic
+    template_name = "lab/methodic_detail.html"
+    context_object_name = "methodic"
+    pk_url_kwarg = "methodic_id"
