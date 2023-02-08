68a5ad88b2824602d02308b9b5254463d5e67fed
LemanRus <wlad163163@gmail.com>
2023-02-08 15:02:34 +0400

    Add listed views to reagents and methodics

--- lims/lab/urls.py
+++ lims/lab/urls.py
@@ -7,4 +7,6 @@ app_name = 'lab'
 
 urlpatterns = [
     path('', TemplateView.as_view(), name='home'),
+    path('reagents/', views.ReagentsListView.as_view(), name='reagents'),
+    path('methodics/', views.MethodicsListView.as_view(), name='methodics'),
 ]
--- lims/lab/views.py
+++ lims/lab/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from .models import Reagent, Methodic
+
+
+class ReagentsListView(ListView):
+    model = Reagent
+    template_name = "lab/reagents_list.html"
+    context_object_name = "reagents"
+
+
+class MethodicsListView(ListView):
+    model = Methodic
+    template_name = "lab/methodics_list.html"
+    context_object_name = "methodics"
+
+#
+# class ReagentDetailView(DetailView):
+#     model = Reagent
+#     template_name = "lab/reagents_list.html"
+#     context_object_name = "reagents"
+#
+#
+# class MethodicDetailView(DetailView):
+#     model = Methodic
+#     template_name = "lab/methodics_list.html"
+#     context_object_name = "methodics"
