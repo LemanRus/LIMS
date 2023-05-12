d1d8c853459e17257d80443f7a908d454d9b81ed
LemanRus <wlad163163@gmail.com>
2023-05-12 21:12:04 +0400

    Add verbose names to fields in models

--- lims/lab/models.py
+++ lims/lab/models.py
@@ -5,11 +5,13 @@ from simple_history.models import HistoricalRecords
 
 
 class Methodic(models.Model):
-    name = models.CharField(max_length=200)
-    acts_from = models.DateField(blank=True, null=True)
-    acts_to = models.DateField(blank=True, null=True)
-    used_reagents = models.ManyToManyField('Reagent', blank=True, related_name='methodics')
-    used_equipment = models.ManyToManyField('Equipment', blank=True, related_name='methodics')
+    name = models.CharField(max_length=200, verbose_name='Наименование')
+    acts_from = models.DateField(blank=True, null=True, verbose_name='Введена в')
+    acts_to = models.DateField(blank=True, null=True, verbose_name='Действительна до')
+    used_reagents = models.ManyToManyField('Reagent', blank=True, related_name='methodics',
+                                           verbose_name='Используемые реактивы')
+    used_equipment = models.ManyToManyField('Equipment', blank=True, related_name='methodics',
+                                            verbose_name='Используемое оборудование')
 
     def __str__(self):
         return f'Методика "{self.name}"'
@@ -20,9 +22,9 @@ class Methodic(models.Model):
 
 
 class Reagent(models.Model):
-    name = models.CharField(max_length=200)
-    made_by = models.CharField(max_length=200)
-    made_date = models.DateField(blank=True, null=True)
+    name = models.CharField(max_length=200, verbose_name='Наименование')
+    made_by = models.CharField(max_length=200, verbose_name='Изготовитель')
+    made_date = models.DateField(blank=True, null=True, verbose_name='Дата производства')
     best_before = models.DateField(blank=True, null=True, verbose_name='Годен до')
 
     history = HistoricalRecords()
@@ -36,10 +38,10 @@ class Reagent(models.Model):
 
 
 class Equipment(models.Model):
-    name = models.CharField(max_length=200)
-    last_cal = models.DateField()
-    next_cal = models.DateField()
-    cal_organisation = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, verbose_name='Наименование')
+    last_cal = models.DateField(verbose_name='Дата последней калибровки (поверки)')
+    next_cal = models.DateField(verbose_name='Дата следующей калибровки (поверки)')
+    cal_organisation = models.CharField(max_length=200, verbose_name='Организация, проводящая калибровку (поверку)')
 
     def __str__(self):
         return self.name
@@ -50,11 +52,12 @@ class Equipment(models.Model):
 
 
 class TechnicalMaintenance(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, verbose_name='Наименование')
     type = models.CharField(choices=[('Плановое', 'Плановое'), ('Периодическое', 'Периодическое'),
-                                     ('Внеплановое', 'Внеплановое')], max_length=60)
-    next_date = models.DateField()
-    equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE, related_name='maintenance')
+                                     ('Внеплановое', 'Внеплановое')], max_length=60, verbose_name='Тип техобслуживания')
+    next_date = models.DateField(verbose_name='Дата следующего техобслуживания')
+    equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE, related_name='maintenance',
+                                  verbose_name='Относится к оборудованию')
 
     def __str__(self):
         return f'{self.name}, {self.type}'
@@ -65,11 +68,11 @@ class TechnicalMaintenance(models.Model):
 
 
 class Contract(models.Model):
-    number = models.CharField(max_length=200)
-    contragent = models.CharField(max_length=200)
-    date_conclusion = models.DateField()
-    date_end = models.DateField()
-    file_contract = models.FileField(blank=True, null=True)
+    number = models.CharField(max_length=200, verbose_name='Номер договора')
+    contragent = models.CharField(max_length=200, verbose_name='С кем заключен договор')
+    date_conclusion = models.DateField(verbose_name='Дата заключения')
+    date_end = models.DateField(verbose_name='Дата окончания')
+    file_contract = models.FileField(blank=True, null=True, verbose_name='Файл договора (скан)')
 
     def __str__(self):
         return f'Договор {self.number} с «{self.contragent}»'
@@ -85,14 +88,15 @@ class Contract(models.Model):
 
 
 class Protocol(models.Model):
-    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='protocols', default=CustomUser.objects.first().pk)
-    contract = models.ForeignKey('Contract', on_delete=models.CASCADE, related_name='protocols')
-    number = models.CharField(max_length=200)
-    act_number = models.CharField(max_length=200)
-    used_methodics = models.ManyToManyField(Methodic, related_name='protocols')
-    file_protocol = models.FileField(blank=True, null=True)
-    file_act = models.FileField(blank=True, null=True)
-    close_date = models.DateTimeField()
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='protocols',
+                               default=CustomUser.objects.first().pk, verbose_name='Автор')
+    contract = models.ForeignKey('Contract', on_delete=models.CASCADE, related_name='protocols', verbose_name='К договору')
+    number = models.CharField(max_length=200, verbose_name='Номер протокола')
+    act_number = models.CharField(max_length=200, verbose_name='Номер акта')
+    used_methodics = models.ManyToManyField(Methodic, related_name='protocols', verbose_name='Используемые методики')
+    file_protocol = models.FileField(blank=True, null=True, verbose_name='Файл протокола')
+    file_act = models.FileField(blank=True, null=True, verbose_name='Файл акта')
+    close_date = models.DateTimeField(verbose_name='Дата закрытия')
 
     def __str__(self):
         return f'Протокол {self.number}'
@@ -113,11 +117,11 @@ class Protocol(models.Model):
 
 
 class Bid(models.Model):
-    contract = models.ForeignKey('Contract', on_delete=models.CASCADE, related_name='bids')
-    number = models.CharField(max_length=200)
-    invoices = models.ManyToManyField('Invoice', related_name='bids')
+    contract = models.ForeignKey('Contract', on_delete=models.CASCADE, related_name='bids', verbose_name='К договору')
+    number = models.CharField(max_length=200, verbose_name='Номер заявки')
+    invoices = models.ManyToManyField('Invoice', related_name='bids', verbose_name='Счета')
     status = models.CharField(choices=[('c', 'Исполнена'), ('r', 'Отклонена'),
-                                     ('p', 'Выполняется')], max_length=60, default='p')
+                                       ('p', 'Выполняется')], max_length=60, default='p', verbose_name='Статус')
 
     def __str__(self):
         return f'Заявка {self.number} ({self.get_status_display()})'
@@ -128,9 +132,9 @@ class Bid(models.Model):
 
 
 class Invoice(models.Model):
-    number = models.CharField(max_length=200)
+    number = models.CharField(max_length=200, verbose_name='Номер счёта')
     status = models.CharField(choices=[('p', 'Оплачен'), ('b', 'Задолженность'),
-                                       ('w', 'В процессе')], max_length=60, default='w')
+                                       ('w', 'В процессе')], max_length=60, default='w', verbose_name='Статус')
 
     def __str__(self):
         return f'Счёт {self.number} ({self.get_status_display()})'
@@ -146,13 +150,12 @@ def notebook_record_file_path(instance, filename):
 
 
 class Record(models.Model):
-    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='records')
-    text = models.TextField(max_length=5000)
-    file = models.FileField(upload_to=notebook_record_file_path, blank=True)
-    date_created = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='records', verbose_name='Автор')
+    text = models.TextField(max_length=5000, verbose_name='Текст записи')
+    file = models.FileField(upload_to=notebook_record_file_path, blank=True, verbose_name='Приложенный файл')
+    date_created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
 
     @property
     def file_record_url(self):
         if self.file and hasattr(self.file, 'url'):
             return self.file.url
-
