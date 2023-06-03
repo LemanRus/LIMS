97d76c767670057dd50a9d76fc931071e10d5b85
LemanRus <wlad163163@gmail.com>
2023-06-03 22:26:43 +0400

    Add lost history field to Reagent model

--- lims/lab/models.py
+++ lims/lab/models.py
@@ -29,6 +29,8 @@ class Reagent(models.Model):
     made_date = models.DateField(blank=True, null=True, verbose_name='Дата производства')
     best_before = models.DateField(blank=True, null=True, verbose_name='Годен до')
 
+    history = HistoricalRecords()
+
     def __str__(self):
         return self.name
 
